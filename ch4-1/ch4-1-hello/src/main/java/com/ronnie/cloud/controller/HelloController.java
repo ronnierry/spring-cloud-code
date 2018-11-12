6ca30d5cf5f065e0f78448ca4d4f91c9df09b93a
rongyu <ronniery@aliyun.com>
2018-11-12 13:50:55 +0800

    FeignClient's Demo

---
+++ ch4-1/ch4-1-hello/src/main/java/com/ronnie/cloud/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.ronnie.cloud.controller;
+
+import com.ronnie.cloud.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @Description:
+ * @Author: rongyu
+ * @CreateDate: 2018/11/12$ 13:40$
+ * @Remark:
+ */
+@RestController
+public class HelloController {
+    @Autowired
+    private HelloService helloService;
+
+    @GetMapping("/query")
+    public String query(String q) {
+        return helloService.searchRepo(q);
+    }
+}
